69b7c12ca11e23e4857923768e02cc72ea36cbb4
Daniel Brown <dsbrown@cs.utexas.edu>
2018-08-27 21:58:36 -0500

    plotting code for mcar

--- scripts/plot_mcar_basic_exp.py
+++ scripts/plot_mcar_basic_exp.py
@@ -5,33 +5,40 @@ Created on Tue Aug 14 16:08:41 2018
 @author: dsbrown
 """
 import numpy as np
-
-demo_data = False
-learned_data = False
-demos = []
-learned = []
-reader = open("data/mountain_car_test_data.txt")
-for line in reader:
-    if line.startswith("#demos"):
-        demo_data = True
-        learned_data = False
-    elif line.startswith("#learned"):
-        demo_data = False
-        learned_data = True
-    elif demo_data:
-        demos.append(float(line))
-    elif learned_data:
-        learned.append(float(line))
-        
 import matplotlib.pyplot as plt
-plt.plot(range(1,len(demos)+1),demos, label="Demonstrator", lw=2)
-plt.plot([1, len(demos)], np.mean(learned) * np.ones(2), '--', label="LfLD", lw=2)
-plt.xlabel("Iteration",fontsize=18)
-plt.ylabel("Return",fontsize=18)
-plt.xticks([1,5,10,15,20],fontsize=18) 
-plt.axis([0,21,-1100,0])
+demonstrations = [5,10,20,30,40,50]
+num_reps = 20
+birl_conf = 1.0
+#plot MWAL
+mwal = []
+for demos in demonstrations:
+    mwal_returns = []
+    for seed in range(num_reps):
+        reader = open("data/mcar_mwal_seed" + str(seed) + "_demos" + str(demos))
+        for line in reader:
+            mwal_returns.append(float(line)) 
+    mwal.append(-np.mean(mwal_returns))
+
+#plot BIRL
+birl = []
+for demos in demonstrations:
+    birl_returns = []
+    for seed in range(num_reps):
+        reader = open("data/mcar_birl_conf" + str(birl_conf) + "_seed" + str(seed) + "_demos" + str(demos))
+        for line in reader:
+            birl_returns.append(float(line)) 
+    birl.append(-np.mean(birl_returns))
+
+  
+
+plt.plot(demonstrations, mwal, label="GT-IRLfL", lw=3)
+plt.plot(demonstrations, birl, label="BIRL", lw=3)
+plt.xlabel("Number of demonstrations",fontsize=18)
+plt.ylabel("Average steps to goal",fontsize=18)
+plt.xticks(demonstrations,fontsize=18) 
+#plt.axis([0,21,-1100,0])
 plt.yticks(fontsize=18) 
-plt.legend(loc='best', fontsize=19)
+plt.legend(loc='best', fontsize=18)
 plt.tight_layout()
 
 plt.show()
